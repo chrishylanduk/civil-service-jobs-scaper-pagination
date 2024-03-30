d007861954d0162fb5a1d7bf816783343694707c
Chris Hyland <95708546+chrishylanduk@users.noreply.github.com>
2024-03-30 22:15:23 +0000

    feature: retry whole scrape if exception

--- src/main/java/com/chrishyland/wholewebsitescraper/service/WholeWebsiteScraperCommandLineRunner.java
+++ src/main/java/com/chrishyland/wholewebsitescraper/service/WholeWebsiteScraperCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.chrishyland.wholewebsitescraper.service;
 
 import com.chrishyland.wholewebsitescraper.domain.service.PageScrapingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("com.chrishyland.wholewebsitescraper.infrastructure")
 @EntityScan("com.chrishyland.wholewebsitescraper.infrastructure")
+@Slf4j
 public class WholeWebsiteScraperCommandLineRunner implements CommandLineRunner {
 
     private final PageScrapingService pageScrapingService;
@@ -23,8 +25,31 @@ public class WholeWebsiteScraperCommandLineRunner implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        pageScrapingService.scrapePagesInSitemap(System.getenv("SITEMAP_URL"));
+    public void run(String... args) {
+        final int maxRetries = 3;
+        int attempt = 0;
+
+        while (attempt < maxRetries) {
+            try {
+                attempt++;
+                pageScrapingService.scrapePagesInSitemap(System.getenv("SITEMAP_URL"));
+                log.info("Scraping successful on attempt {}", attempt);
+                break;
+            } catch (Exception e) {
+                log.error("Failed to scrape pages from sitemap at attempt {}", attempt);
+                if (attempt < maxRetries) {
+                    log.info("Waiting 2 minutes before retrying");
+                    try {
+                        Thread.sleep(120000);
+                    } catch (InterruptedException ie) {
+                        System.out.println("Interrupted during wait before retry" + ie);
+                        break;
+                    }
+                } else {
+                    log.error("Max retries reached. Aborting scraping.");
+                }
+            }
+        }
     }
 
 }
